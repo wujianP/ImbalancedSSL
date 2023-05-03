71049a235d068617fac6f10dc3593b7c8cd03428
wujianP <1051233700@qq.com>
2023-05-03 16:51:43 +0800

    将DARP改为了DDP训练

--- DARP/mis_dist_train_imagenetLT_fix.py
+++ DARP/mis_dist_train_imagenetLT_fix.py
@@ -28,7 +28,7 @@ parser = argparse.ArgumentParser(description='PyTorch DARP Training')
 
 # Optimization options
 parser.add_argument('--epochs', default=300, type=int, metavar='N', help='number of total epochs to run')
-parser.add_argument('--val_iteration', type=int, default=500, help='Frequency for the evaluation')
+parser.add_argument('--val-iteration', type=int, default=125, help='Frequency for the evaluation')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N', help='manual epoch number (useful on restarts)')
 parser.add_argument('--batch_size', default=64, type=int, metavar='N', help='train batchsize')
 parser.add_argument('--lr', '--learning-rate', default=0.002, type=float, metavar='LR', help='initial learning rate')
