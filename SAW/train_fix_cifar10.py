7e44daa0b0ae548c6eaed2ad0a0a4bc83da6ebed
wujianP <1051233700@qq.com>
2023-07-01 19:01:45 +0800

    add more baselines

--- SAW/train_fix_cifar10.py
+++ SAW/train_fix_cifar10.py
@@ -677,8 +677,8 @@ class WeightEMA(object):
     def step(self):
         one_minus_alpha = 1.0 - self.alpha
         for param, ema_param in zip(self.params, self.ema_params):
-            from IPython import embed
-            embed()
+            ema_param = ema_param.float()
+            param = param.float()
             ema_param.mul_(self.alpha)
             ema_param.add_(param * one_minus_alpha)
             # customized weight decay
