f782ad4c8b461836b8c66dea7e94b5e3d89bd9a0
wujianP <1051233700@qq.com>
2023-07-02 16:53:34 +0800

    add more baselines

--- DASO/train_imagenetLT.sh
+++ DASO/train_imagenetLT.sh
@@ -1,11 +1,18 @@
 conda activate /discobox/wjpeng/env/bmb/
 FOLDERDIR="/discobox/wjpeng/ckp/BMB/rebuttal/baselines/imagenetLT/DASO_20per"
+DATA_PATH='/dev/shm/imagenet'
+ANN_PATH='/discobox/wjpeng/code/img127-tcp/dataset/ImageNet_LT'
 mkdir $FOLDERDIR
 cd /discobox/wjpeng/code/ImbalancedSSL/DASO_imageNet
 
 python main.py \
 --config-file configs/fixmatch_daso_imagenetLT.yaml \
+DATASET.LABELED_RATIO 20 \
 GPU_ID 5 \
+SOLVER.MAX_ITER 15000 \
+ALGORITHM.CONFIDENCE_THRESHOLD 0.7 \
 SOLVER.UNLABELED_BATCH_RATIO 1 \
 OUTPUT_DIR ${FOLDERDIR} \
+DATASET.DATA_PATH $DATA_PATH \
+DATASET.ANN_PATH $ANN_PATH \
  | tee $FOLDERDIR/progress.txt
