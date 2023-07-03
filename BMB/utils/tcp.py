78f609939e9168a4e2da08a1d0571e38b7237d99
wujianP <1051233700@qq.com>
2023-07-03 17:27:20 +0800

    add more baselines

--- BMB/utils/tcp.py
+++ BMB/utils/tcp.py
@@ -35,9 +35,6 @@ class TailClassPool(object):
         input_labels = input_labels.detach()
 
         B, C = input_features.shape[0], class_distribution.shape[0]
-        print(input_labels)
-        print(B)
-        print(C)
         input_onehot = label2onehot(label=input_labels, batch=B, num_class=C, to_cuda=True)
 
         # 按照类别样本数量分布，选取本次需要加入的样本
