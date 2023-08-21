8af9bc6c79c2662d5818eaa4ed53afc805bbc02c
wujianP <1051233700@qq.com>
2023-08-21 13:22:47 +0800

    add prompt to prompt

--- ABC/mis_cifar10.sh
+++ ABC/mis_cifar10.sh
@@ -1,16 +1,16 @@
 conda activate /discobox/wjpeng/env/bmb/
-FOLDERDIR='/discobox/wjpeng/ckp/BMB/rebuttal/mismatch/ABC_cifar10_rl50_ru1_seed1'
+FOLDERDIR='/discobox/wjpeng/ckp/BMB/rebuttal/mismatch/ABC_cifar10_rl50_ru100_seed2'
 cd /discobox/wjpeng/code/ImbalancedSSL/ABC
 mkdir $FOLDERDIR
 
 python ABCfix.py \
---gpu 4 \
---manualSeed 1 \
+--gpu 3 \
+--manualSeed 2 \
 --num_max_l 1500 \
 --num_max_u 3000 \
 --dataset cifar10 \
 --imb_ratio_l 50 \
---imb_ratio_u 1 \
+--imb_ratio_u 100 \
 --epoch 500 \
 --val-iteration 500 \
 --out $FOLDERDIR
